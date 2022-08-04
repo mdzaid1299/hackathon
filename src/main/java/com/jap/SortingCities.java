f2076f7885151e79adbd415598d2c8620ed57190
787577_MOHD <mdzaid1299@gmail.com>
2022-08-04 10:25:46 +0000

    Saving changes from StackRoute workbench on Thu Aug 04 2022 15:55:44 GMT+0530 (India Standard Time)

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -2,12 +2,33 @@ package com.jap;
 
 /**
  * Hello world!
- *
  */
-public class SortingCities
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class SortingCities {
+    public static void main(String[] args) {
+        String[] cityName = {"Bern", "Lucerne", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch" };
+        int[] distance = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+        String[] toUpperCase = upperCase(cityName);
+        for (int i = 0; i < toUpperCase.length; i++) {
+            System.out.println(toUpperCase[i]);
+        }
+    }
+
+    public static String[] upperCase(String[] cityName) { // The Cities are not given in same case, convert all the cities in uppercase.
+        String[] newTemp = new String[cityName.length];
+        if (cityName.length == 0) {
+            return null;
+        }
+        for (int i = 0; i < cityName.length; i++) {
+            newTemp[i] = cityName[i].toUpperCase();
+        }
+        return newTemp;
+    }
+
+    //   Find the city which is 52 kms from Zurich
+    public static String[] findCity(String[]distance,int[]kMCheck) {
+        
+        for (int i = 0; i < distance.length; i++) {
+
+        }
     }
 }
